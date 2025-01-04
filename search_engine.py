9c45785124c9cd3198ecf433d8dff5127e2d071e
lml lklknlk <you@example.com>
2025-01-04 12:28:10 +0100

    feat: nicer display of query results

--- search_engine.py
+++ search_engine.py
@@ -12,25 +12,25 @@ def internal_error(exception):
 
 @app.route("/")
 def start():
-    return "<form action='reversed' method='get'><input name='rev'></input></form>"
+    return "<form action='search_for_pages' method='get'><input name='search_query'></input></form>"
 
-@app.route("/reversed")
-def reversed():
-    query = request.args['rev']
+@app.route("/search_for_pages")
+def search_for_pages():
+    query = request.args['search_query']
     search_results = get_relevant_links(query)
+    
     #return_string = "<h1>"
-    return_string = ""
-    for i in range(len(search_results)):
-        return_string += '<a href = "' + search_results[i]["page_url"] + '">'
-        return_string += search_results[i]["title"]
-        return_string += '</a><br>'
-        return_string += '<p>' + search_results[i]["content_summary"] + '</p><br>'
-        #return_string += " "
-    #return_string += "</h1>"
-
-    if return_string != "": 
-        return return_string
+    return_string = "<html><head></head><body><div id='header' align='center'><form action='search_for_pages' method='get'><input name='search_query' value=" + query + "></input></form></div>"
+    return_string += "<div id='main-content' style='width: 50%; margin-left: auto; margin-right: auto;'>"
+    if len(search_results)>0:
+        for i in range(len(search_results)):
+            return_string += '<a href = "' + search_results[i]["page_url"] + '">'
+            return_string += search_results[i]["title"]
+            return_string += '</a>'
+            return_string += '<p>' + search_results[i]["content_summary"] + '</p><br>'
     else:
-        return "<h1>no results found</h1>"
-    #return '<a href = "google.com">123</a>'
-    #return "<h1>"+request.args['rev'][::-1]+"NIOBA"
+        return_string += "<h3>no results found</h3>"
+    return_string += "</div></body></html>"
+    return return_string
+    
+ 
